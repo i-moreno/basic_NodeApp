ca6106b5594aeb7f758d0cbd986af051d2618fda
Carlos <morenocarlos9973@gmail.com>
2020-01-21 21:59:59 -0600

    Install nodemon and change some text on the views

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 // Separate handler logic from router logic
 
 exports.homeGreeting =  function(req, res, next) {
-  res.render('index', { title: 'Welcome to Express' });
+  res.render('index', { title: 'Welcome to Express', subtitle: 'Backend Cornerstone' });
 }
