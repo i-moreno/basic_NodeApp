ae364a5d12bedf8987aca252139ae7f293f326b5
Carlos <morenocarlos9973@gmail.com>
2020-01-22 22:42:55 -0600

    Retrieving all leads.

--- controllers/landing.js
+++ controllers/landing.js
@@ -11,9 +11,16 @@ exports.get_landing =  function(req, res, next) {
 exports.get_lead =  function(req, res, next) {
   console.log('--> Lead Email:', req.body.lead_email);
   
-  return  models.Lead.create({
-    email: req.body.lead_email
-  }).then(lead => {
-    res.redirect('/');
+  return models.Lead.create({
+		email: req.body.lead_email
+	}).then(lead => {
+		res.redirect('/leads');
+	})
+}
+
+exports.show_leads =  function(req, res, next) {
+  return models.Lead.findAll().then(leads => {
+    res.render('landing', { title: 'Welcome to Express', leads: leads });
   })
+  
 }
