c7a7f9037b9427a91c7f4114943e31489497862e
Carlos <morenocarlos9973@gmail.com>
2020-02-05 12:10:34 -0600

    Add passport configuration

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,36 @@
+var models = require("../models");
+var bcrypt = require("bcrypt");
+var passport = require("passport");
+var myPassport = require("../passport_setup")(passport);
+var flash = require("connect-flash");
+
 exports.show_login = function(req, res, next) {
 	res.render('user/login', { formData: {}, errors: {} });
 }
 
 exports.show_signup = function(req, res, next) {
 	res.render('user/signup', { formData: {}, errors: {} });
+}
+
+exports.signup = function(req, res, next) {
+   const newUser = models.User.build({
+     email: req.body.email,
+     password: generateHash(req.body.password)
+   });
+   
+   return newUser.save().then(result => {
+     passport.authenticate('local', {
+       successRedirect: "/",
+       failureRedirect: "/singup",
+       failureFlash: true
+     })(req, res, next);
+   })
+}
+
+exports.login = function(req, res, next) {
+  
+}
+
+const generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.getSaltSync(8), null);
 }
